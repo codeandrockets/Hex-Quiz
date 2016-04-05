e37c08e7fe157909b786fab81e393487dd3f7757
Steven Jay <stevenjasionowicz@gmail.com>
2016-04-06 00:01:10 +1200

    Basics are done

--- script.js
+++ script.js
@@ -20,18 +20,97 @@
 		var hex3 = generateHex();
 		var hex4 = generateHex();
 
-		document.getElementById('div1').style.background = "#" + hex1;
-		document.getElementById('div2').style.background = "#" + hex2;
-		document.getElementById('div3').style.background = "#" + hex3;
-		document.getElementById('div4').style.background = "#" + hex4;
-
 		var array = [hex1, hex2, hex3, hex4];
-		console.log(array);
-		var rightHex = array[Math.floor(Math.random() * array.length)];
-		console.log(rightHex);
-		document.getElementById('rightHex').innerHTML = "#" + rightHex;
+
+		return array;
 
 	}
 
 
+	function populate() {
+
+		document.getElementById("answer").innerHTML = "";
+		document.getElementById('checkColor').style.background = "#ffffff";
+
+		var array = fillBoxes();
+		
+		document.getElementById('div1').style.background = "#" + array[0];
+		document.getElementById('div2').style.background = "#" + array[1];
+		document.getElementById('div3').style.background = "#" + array[2];
+		document.getElementById('div4').style.background = "#" + array[3];
+		
+		var rightHex = array[Math.floor(Math.random() * array.length)];
+		document.getElementById('rightHex').innerHTML = "#" + rightHex;
+	}
+
+	function check1() {
+
+		var element = document.getElementById('div1');
+		var style = window.getComputedStyle(element);
+		var hex = style.getPropertyValue('background-color');
+		console.log(hex);
+
+		var correctHex = document.getElementById("rightHex").innerHTML;
+		document.getElementById('checkColor').style.background = correctHex;
+
+		if ( document.getElementById('checkColor').style.background === document.getElementById('div1').style.background ) {
+			document.getElementById("answer").innerHTML = "Correct";
+			} else {
+				document.getElementById("answer").innerHTML = "Wrong";
+			}
+	}
+
+	function check2() {
+
+		var element = document.getElementById('div2');
+		var style = window.getComputedStyle(element);
+		var hex = style.getPropertyValue('background-color');
+		console.log(hex);
+
+		var correctHex = document.getElementById("rightHex").innerHTML;
+		document.getElementById('checkColor').style.background = correctHex;
+
+		if ( document.getElementById('checkColor').style.background === document.getElementById('div2').style.background ) {
+			document.getElementById("answer").innerHTML = "Correct";
+			} else {
+				document.getElementById("answer").innerHTML = "Wrong";
+			}		
+		
+	}
+
+	function check3() {
 	
+		var element = document.getElementById('div3');
+		var style = window.getComputedStyle(element);
+		var hex = style.getPropertyValue('background-color');
+		console.log(hex);
+
+		var correctHex = document.getElementById("rightHex").innerHTML;
+		document.getElementById('checkColor').style.background = correctHex;
+
+		if ( document.getElementById('checkColor').style.background === document.getElementById('div3').style.background ) {
+				document.getElementById("answer").innerHTML = "Correct";
+			} else {
+				document.getElementById("answer").innerHTML = "Wrong";
+			}		
+
+
+	}
+
+	function check4() {
+	
+		var element = document.getElementById('div4');
+		var style = window.getComputedStyle(element);
+		var hex = style.getPropertyValue('background-color');
+		console.log(hex);
+
+		var correctHex = document.getElementById("rightHex").innerHTML;
+		document.getElementById('checkColor').style.background = correctHex;
+
+		if ( document.getElementById('checkColor').style.background === document.getElementById('div4').style.background ) {
+				document.getElementById("answer").innerHTML = "Correct";
+			} else {
+				document.getElementById("answer").innerHTML = "Wrong";
+			}	
+
+	}
